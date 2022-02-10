bf9bfa22af3f6709a717c2071bd04e8d12a19bbe
featztex <romaem3019@gmail.com>
2022-02-10 23:02:55 +0300

    added sigfillset

--- 15.c
+++ 15.c
@@ -22,9 +22,10 @@ int main(void) {
         .sa_flags = SA_RESTART
     };
 
-    // обнулим все остальные поля
-    sigemptyset(&sigact_struct.sa_mask);
-
+    // ___________
+    sigfillset(&sigact_struct.sa_mask);
+    
+    // ___________
     for(int signal = 1; signal <= NSIG; signal++) {
         if(sigaction(signal, &sigact_struct, NULL) == -1) {
             psignal(signal, NULL);
@@ -32,33 +33,14 @@ int main(void) {
     }
 
     siginfo_t  info;
-    sigset_t   mask;
+    sigset_t mask;
 
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGINT);
-    sigaddset(&mask, SIGQUIT);
-    sigaddset(&mask, SIGTERM);
-    sigaddset(&mask, SIGTSTP);
+    sigfillset(&mask);
 
-    /* sigaddset(&mask, SIGILL);
-    sigaddset(&mask, SIGABRT);
-    sigaddset(&mask, SIGFPE);
-    sigaddset(&mask, SIGSEGV);
-    sigaddset(&mask, SIGPIPE);
-    sigaddset(&mask, SIGALRM);
-    sigaddset(&mask, SIGUSR1);
-    sigaddset(&mask, SIGUSR2);
-    sigaddset(&mask, SIGCHLD);
-    sigaddset(&mask, SIGCONT);
-    sigaddset(&mask, SIGSTOP);
-    sigaddset(&mask, SIGTTIN);
-    sigaddset(&mask, SIGTTOU); */
-
-    // прога не падает потому что сигнал попал в обработчик
+    // ____________
     while(1) {
         int signum = sigwaitinfo(&mask, &info);
-        sig_handler(signum);
-        printf(" last_signal = %d (%s)\n", g_last_signal, strsignal(g_last_signal));
+        printf(" last_signal = %d (%s)\n", signum, strsignal(signum));
     }
     return 0;
 }
